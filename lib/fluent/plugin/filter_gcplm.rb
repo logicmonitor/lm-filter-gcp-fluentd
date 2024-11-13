93727232ef7b1ee1f6f3377005a46c132e4a9891
Vishal Charpe <vishal.charpe@logicmonitor.com>
2024-11-13 09:31:54 +0530

    add-resourcetype:  add-resourcetype-gcp
    add-resourcetype:  add-resourcetype-gcp
    
    add-resourcetype:  add-resourcetype-gcp
    
    add-resourcetype:  add-resourcetype-gcp

--- lib/fluent/plugin/filter_gcplm.rb
+++ lib/fluent/plugin/filter_gcplm.rb
@@ -5,7 +5,7 @@ module Fluent::Plugin
     Fluent::Plugin.register_filter('gcplm', self)
 
     METADATA_KEYS_TO_RENAME = {"trace" => "trace_id", "spanId" => "span_id","resource.type" => "_type", "severity" => "log_level"}.freeze
-    STATIC_METADATA = {"_integration" => "gcp"}
+    STATIC_METADATA = {"_integration" => "gcp", "_resource.type" => "GCP"}
     LM_TENANT_ID_KEY = "_lm.tenantId".freeze
 
     config_param :metadata_keys, :array, default: ["severity", "logName", "labels", "resource.type", "resource.labels", "httpRequest"], value_type: :string
